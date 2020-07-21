7146431f0b1790876ebd440feee77e1c43bed046
cproudfoot92 <50339446+cproudfoot92@users.noreply.github.com>
2020-07-21 17:52:25 +0100

    task1 complete

--- task1/data_cleaning_scripts/task1_data_cleaning_script.R
+++ task1/data_cleaning_scripts/task1_data_cleaning_script.R
@@ -5,16 +5,23 @@ library(janitor)
 #Read in dirty decathlon data
 decathlon_original <- read_rds("raw_data/decathlon.rds")
 #clean variable names
-decathlon_clean_names <- clean_names(decathlon_original)
-
-decathalon_col_names <- rownames_to_column(decathlon_clean_names, var = "name")
-
-decacthalon_arr <- decathalon_col_names %>%
+decathlon_clean <- decathlon_original %>%
+  # put row names into a variable
+  rownames_to_column( var = "name") %>%
   arrange(name) %>%
-  mutate(name = tolower(name))
+  # make all names lower case
+  mutate(name = tolower(name)) %>%
+  # create id number for every unique name
+  mutate(id = group_indices(.,name)) %>%
+  # id to the front
+  select(id, everything()) %>%
+  clean_names()
 
-id <- decacthalon_arr %>%
-  mutate(id = group_indices(.,name))
 
-id_moved <- id %>%
-  select(id, everything())
+# trimmed data set for analysis
+decathlon_trimmed <- decathlon_clean %>%
+  select(id, name, x100m, long_jump, shot_put, x400m, rank, points, competition)
+
+
+# save clean trimmed data as .csv
+write_csv(decathlon_trimmed, "clean_data/decathlon_trimmed.csv")
