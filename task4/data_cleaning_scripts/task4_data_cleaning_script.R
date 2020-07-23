4ff45b344a1ca78d4c175771a3cf479feb12bdf6
cproudfoot92 <50339446+cproudfoot92@users.noreply.github.com>
2020-07-23 15:28:28 +0100

    task4 cleaning

--- task4/data_cleaning_scripts/task4_data_cleaning_script.R
+++ task4/data_cleaning_scripts/task4_data_cleaning_script.R
@@ -97,6 +97,7 @@ boing_candy_long <- boing_candy_wide %>%
                names_to = "candy",
                values_to = "rating",
                names_prefix = "q6_")%>%
+
   # Filter out any non candy items.
   filter(!candy %in% c("abstained_from_m_ming", 
                        "bonkers_the_board_game",
@@ -124,11 +125,75 @@ boing_candy_long <- boing_candy_wide %>%
                        "vicodin",
                        "whatchamacallit_bars",
                        "white_bread",
-                       "whole_wheat_anything"))
+                       "whole_wheat_anything",
+                       "candy_that_is_clearly_just_the_stuff_given_out_for_free_at_restaurants",
+                       "joy_joy_mit_iodine_2",
+                       "joy_joy_mit_iodine")) %>%
+  
+# tidying candy names and combining duplicates.
+  mutate(candy = 
+           recode(candy, 
+                  "anonymous_brown_globs_that_come_in_black_and_orange_wrappers" = "mary_janes",
+                  "anonymous_brown_globs_that_come_in_black_and_orange_wrappers_a_k_a_mary_janes" = "mary_janes",
+                  "bonkers_the_candy" = "bonkers",
+                  "boxo_raisins" = "box_o_raisins",
+                  "chick_o_sticks_we_don_t_know_what_that_is" = "chick_o_sticks",
+                  "dark_chocolate_hershey" = "hersheys_dark_chocolate",
+                  "jolly_rancher_bad_flavor" = "jolly_ranchers",
+                  "jolly_ranchers_good_flavor" = "jolly_ranchers",
+                  "licorice_not_black" = "licorice",
+                  "licorice_yes_black" = "licorice",
+                  "nown_laters" = "now_and_laters",
+                  "sea_salt_flavored_stuff_probably_chocolate_since_this_is_the_it_flavor_of_the_year" = "sea_salt_flavored_chocolate",
+                  "smarties_american" = "smarties",
+                  "smarties_commonwealth" = "smarties",
+                  "sourpatch_kids_i_e_abominations_of_nature" = "sourpatch_kids",
+                  "sweetums_a_friend_to_diabetes" = "sweetums",
+                  "those_odd_marshmallow_circus_peanut_things" = "circus_peanut",
+                  "tolberone_something_or_other" = "tolberone",
+                  "x100_grand_bar" = "100_grand_bar",
+                  "york_peppermint_patties_ignore" = "york_peppermint_patties")
+         ) 
+age_clean <- boing_candy_long %>%
+  mutate(clean_age = 
+           str_extract(age, "[1-9]*")
+         ) %>%
+  mutate(clean_age = 
+           case_when(clean_age > 122, NA))
+
+ordered_age <- age_clean %>%
+  select(clean_age) %>%
+  arrange(clean_age) %>%
+  distinct(clean_age)
+  
+# clean_countries <- boing_candy_long %>%
+#   mutate(age = if_else(age == NA & str_detect(country, "[1-9]"), country, as.character(NA))
+#          ) %>%
+#   mutate(country =
+#            str_to_lower(country)
+#          ) %>%
+#   mutate(country = 
+#            str_remove_all(country, "[[:punct:]]"))
+  
+age_country <- boing_candy_long %>%
+  filter(str_detect(country, "[1-9]"))
+  
+  # Looked into integrating the other joy/despair columns into the candy but there were far too many entries to clean.
+  # mutate(
+  #   other_joy = str_split(other_joy, ",")
+  # ) %>%
+  # unnest(other_joy) %>%
+  # mutate(other_joy = tolower(other_joy)) %>%
+  # select(-other_dispair, -other_joy)
 
 
-ordered_candy <- boing_candy_long %>%
-  select(candy) %>%
-  arrange(candy) %>%
-  distinct(candy)
+ordered_country <- clean_countries %>%
+  select(country) %>%
+  arrange(country) %>%
+  distinct(country)
+
+# us_pattern <- "[u]*[. ][a]"
+# 
+# usa <- boing_candy_long$country %>%
+#   str_detect(us_pattern)
 
